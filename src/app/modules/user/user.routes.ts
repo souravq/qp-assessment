0e45d78070a8bbdac63a76fbdab08f981bf8fdb7
souravq <souravbera515@gmail.com>
2024-04-10 12:39:49 +0530

    User role auth added in user routes

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import { UserController } from "./user.controller";
+import { AuthGrard } from "../../middlewares/auth";
 const router = express.Router();
 
 // View the list of available grocery items
-router.get("/grocery-items", UserController.viewAvailableGroceryItems);
+router.get(
+  "/grocery-items",
+  AuthGrard.isUser,
+  UserController.viewAvailableGroceryItems
+);
 
 // Ability to book multiple grocery items in a single order
-router.post("/orders", UserController.multipleGroceryItemsOrder);
+router.post(
+  "/orders",
+  AuthGrard.isUser,
+  UserController.multipleGroceryItemsOrder
+);
 
 export const UserRouter = router;
