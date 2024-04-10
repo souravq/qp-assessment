388e4536b9721a5c8750ec1dcf956f247c8c4462
souravq <souravbera515@gmail.com>
2024-04-10 12:39:36 +0530

    Admin role auth added in admin routes

--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -1,18 +1,36 @@
 import express from "express";
 import { AdminController } from "./admin.controller";
+import { AuthGrard } from "../../middlewares/auth";
 const router = express.Router();
 
 // Add new grocery items to the system
-router.post("/grocery-items", AdminController.addNewGroceryItems);
+router.post(
+  "/grocery-items",
+  AuthGrard.isAdmin,
+  AdminController.addNewGroceryItems
+);
 // View existing grocery items
-router.get("/grocery-items", AdminController.viewExistingGroceryItems);
+router.get(
+  "/grocery-items",
+  AuthGrard.isAdmin,
+  AdminController.viewExistingGroceryItems
+);
 // Remove grocery items from the system
-router.delete("/grocery-items/:id", AdminController.removeGroceryItems);
+router.delete(
+  "/grocery-items/:id",
+  AuthGrard.isAdmin,
+  AdminController.removeGroceryItems
+);
 // Update details (e.g., name, price) of existing grocery items
-router.patch("/grocery-items/:id", AdminController.updateGroceryItem);
+router.patch(
+  "/grocery-items/:id",
+  AuthGrard.isAdmin,
+  AdminController.updateGroceryItem
+);
 // Manage inventory levels of grocery items
 router.patch(
   "/grocery-items/:id/manage-inventory",
+  AuthGrard.isAdmin,
   AdminController.manageGroceryItemsInventory
 );
 
