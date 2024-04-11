cc306ad2a2a8ecd1328d66db6fe7abe25348ad92
souravq <souravbera515@gmail.com>
2024-04-11 18:20:48 +0530

    env variable added

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import app from "./app";
 
+const PORT = process.env.PORT || 4000;
+
 async function main() {
-  const server = app.listen(4000, () => {
-    console.log("App is listening on port ", 4000);
+  const server = app.listen(PORT, () => {
+    console.log("App is listening on port ", PORT);
   });
 }
 
